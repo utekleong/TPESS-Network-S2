108c73d50567bb12092802a4ef1eead58cf20017
utek@outlook.com <utek@outlook.com>
2024-11-16 10:34:28 +0800

    updating code

--- scripts/analysis.R
+++ scripts/analysis.R
@@ -5,30 +5,112 @@
 library(tidyverse)
 library(mlVAR)
 
-#loading and cleaning data:
-data_raw <- read.csv("./data/ema_clean.csv")
-data <- data_raw %>%
-  mutate(core = rowMeans(select(data_raw, starts_with("tpess")))) #computing mean score for 10-item TPESS
+#loading baseline data:
+baseline_raw <- read.csv("./data/baseline_clean.csv")
+
+#loading and cleaning EMA data:
+ema_raw <- read.csv("./data/ema_clean.csv")
+ema_clean <- ema_raw %>%
+  mutate(core = rowMeans(select(ema_raw, starts_with("tpess")))) #computing mean score for 10-item TPESS
 
 #removing participants who responded to less than 21 EMAs
-occasion_count <- data %>%
+occasion_count <- ema_clean %>%
   group_by(id) %>%
   filter(has_data == "true") %>%
   count() %>%
   filter(n > 20)
-data <- data %>%
+ema_clean <- ema_clean %>%
   filter(id %in% occasion_count$id) %>% 
   select(id, time, day, occasion, nse, anh:core)
+
+#################################################################
+##                    Baseline descriptives                    ##
+#################################################################
+#subsetting baseline dataset to those retained for analysis
+baseline_clean <- baseline_raw %>% 
+  filter(id %in% occasion_count$id)
+
+baseline_clean <- baseline_clean %>% 
+  mutate(tpess_mean = rowMeans(select(baseline_clean, starts_with("tpess"))))
+
+#cleaning up variable types
+baseline_clean$gender <- as.character(baseline_clean$gender)  
+baseline_clean$diag <- as.character(baseline_clean$diag)
+
+#cleaning up coding
+baseline_clean <- baseline_clean %>% 
+  mutate(
+    
+    #cleaning up coding for gender:
+    gender = case_match(
+      gender,
+      "1" ~ "male",
+      "2" ~ "female",
+      "3" ~ "undisclosed"),
+    
+    #cleaning up coding for ethnicity:
+    ethnicity = case_when(
+      str_detect(ethnicity, regex("chinese", ignore_case = TRUE)) ~ "chinese",
+      str_detect(ethnicity, regex("india", ignore_case = TRUE)) ~ "indian",
+      str_detect(ethnicity, regex("malay", ignore_case = TRUE)) ~ "malay",
+      .default = ethnicity),
+    
+    #cleaning up coding for diagnosis history:
+    diag = case_match(
+      diag,
+      "1" ~ "yes",
+      "2" ~ "no")
+    )
+
+#cleaning up ethnicity data
+baseline_clean$ethnicity <- tolower(baseline_clean$ethnicity)
+
+#sample characteristics
+mean(baseline_clean$age) %>%
+  round(digits = 2)
+sd(baseline_clean$age) %>% 
+  round(digits = 2)
+
+baseline_clean %>% #breakdown of gender
+  count(gender)
+
+baseline_clean %>% #breakdown of past history of diagnosis (>1 year)
+  count(diag)
+
+baseline_clean %>% #breakdown of ethnicity
+  count(ethnicity)
+
+#TPESS descriptives @ baseline
+mean(baseline_clean$tpess_mean) %>%
+  round(digits = 2)
+sd(baseline_clean$tpess_mean) %>%
+  round(digits = 2)
+
+#excluded participants
+baseline_excluded <- baseline_raw  %>% 
+  filter(!(id %in% occasion_count$id))
+
+baseline_excluded <- baseline_excluded %>% 
+  mutate(tpess_mean = rowMeans(select(baseline_excluded, starts_with("tpess"))))
+  
+#TPESS descriptives of excluded
+mean(baseline_excluded$tpess_mean) %>%
+  round(digits = 2)
+sd(baseline_excluded$tpess_mean) %>%
+  round(digits = 2)
+
+t.test(baseline_clean$tpess_mean, baseline_excluded$tpess_mean,
+       alternative = "two.sided")
 
 ##################################################################
 ##                       EMA descriptives                       ##
 ##################################################################
 #how many EMAs were completed in total?:
-data_raw %>% 
+ema_raw %>% 
   count(has_data)
 
 #how many EMAs were completed by participants with >20 measurement occasions:
-ema_20 <- data_raw %>% 
+ema_20 <- ema_raw %>% 
   filter(id %in% occasion_count$id) %>% #this subsets full dataset to the sample of participants who provided >20 measurement occasions.
   filter(has_data == "true")
   
@@ -41,14 +123,14 @@ m_20
 m_20/n_20 #average number of measurement occasions completed
 
 #computing person-specific means of EMA variables:
-person_mean <- data %>% 
+person_mean <- ema_clean %>% 
   select(id,nse, anh:core) %>% 
   group_by(id) %>% 
   summarise_all(~ mean(.x, na.rm = TRUE))
 #write.csv(person_mean, "./data/ema_person_mean.csv")
 
 #computing person-specific SDs of EMA variables:
-person_sd <- data %>% 
+person_sd <- ema_clean %>% 
   select(id,nse, anh:core) %>% 
   group_by(id) %>% 
   summarise_all(~ sd(.x, na.rm = TRUE))
@@ -73,34 +155,34 @@ study_sd <- person_sd %>%
 ##                       Detrending data                       ##
 #################################################################
 #creating new data frame to store residuals:
-data_residuals <- as.data.frame(matrix(0, nrow = nrow(data), ncol = ncol(data)))
+data_residuals <- as.data.frame(matrix(0, nrow = nrow(ema_clean), ncol = ncol(ema_clean)))
 
 #creating variable to iterate over:
-variables <- colnames(data) 
+variables <- colnames(ema_clean) 
 
 #loop to regress time on each variable of interest and extract residuals
 for (v in 5:length(variables)) {
   
   formula <- as.formula(paste0(variables[v], "~ time")) #specifying formula to regress time on each variable of interest; variables[v] returns variable name i.e. "pleasure"
-  regression_detrend <- lm(formula, data = data, na.action = na.exclude) #na.exclude ensures that residuals() will pad output with NAs for ommited cases (to ensure matched length)
+  regression_detrend <- lm(formula, data = ema_clean, na.action = na.exclude) #na.exclude ensures that residuals() will pad output with NAs for ommited cases (to ensure matched length)
   data_residuals[v] <- residuals(regression_detrend) #assigning residuals to corresponding column
   }
 
 #creating new data frame for detrended data:
-data_detrended <- data 
+ema_detrended <- ema_clean 
 
 #replacing observed scores with residuals:
-data_detrended[5:17] <- data_residuals[5:17]
+ema_detrended[5:18] <- data_residuals[5:18]
 
 ##################################################################
 ##                      Network estimation                      ##
 ##################################################################
 # #fitting the network:
-vars <- colnames(data_detrended[5:18])
-# net <- mlVAR(data_detrended, vars = vars,
-#              idvar = "id", dayvar = "day",
-#              temporal = "correlated", contemporaneous = "correlated")
-# saveRDS(net, "./data/RDS/network_correlated.RDS")
+vars <- colnames(ema_detrended[5:18])
+net <- mlVAR(ema_detrended, vars = vars,
+             idvar = "id", dayvar = "day",
+             temporal = "correlated", contemporaneous = "correlated")
+saveRDS(net, "./data/RDS/network_correlated.RDS")
 net <- readRDS("./data/RDS/network_correlated.RDS") #load RDS file instead; fitting network with temporal/contempraneous = "correlated" is extremely computationally intensive
 
 #grouping:
--- scripts/datacleaning.R
+++ scripts/datacleaning.R
@@ -1,20 +1,23 @@
-##################################################################
-##                       Loading packages                       ##
-##################################################################
 #loading required data
 library(tidyverse)
-
 
 #################################################################
 ##                        Baseline data                        ##
 #################################################################
+#loading data
+baseline_raw <- read.csv("./data/baseline_raw.csv", stringsAsFactors = TRUE)
+baseline_clean <- baseline_raw[-c(1,2),]  
 
+#anonymising and retaining relevant variables
+baseline_clean <- baseline_clean %>% 
+  select(id, gender:gadq14)
+#write.csv(baseline_clean, "./data/baseline_clean.csv", row.names = FALSE)
 
 ##################################################################
 ##                           EMA data                           ##
 ##################################################################
 #loading data
-data <- readxl::read_excel("./data/ema_raw.xlsx", sheet = 1)
+ema_data <- readxl::read_excel("./data/ema_raw.xlsx", sheet = 1)
 
 variables <- read.csv("./data/variables.csv")
 
@@ -22,19 +25,19 @@ key <- read.csv("./data/key.csv")
 key$mbl_cod <- as.character(key$mbl_cod)
 
 #matching ids
-data_raw <- full_join(key, data, by = "mbl_cod") 
+ema_raw <- full_join(key, ema_data, by = "mbl_cod") 
 
 #adding "time" variables
-data_raw <- data_raw %>%
-  mutate(date = date(data_raw$scheduled_start_local), 
+ema_raw <- ema_raw %>%
+  mutate(date = date(ema_raw$scheduled_start_local), 
          .after = scheduled_start_local) %>% 
-  mutate(hour = as.character(hour(data_raw$scheduled_start_local)),
+  mutate(hour = as.character(hour(ema_raw$scheduled_start_local)),
          .after = date) %>%
-  mutate(minute = as.character(minute(data_raw$scheduled_start_local)),
+  mutate(minute = as.character(minute(ema_raw$scheduled_start_local)),
          .after = hour)
   
 #day and occasion variables  
-data_raw <- data_raw %>% 
+ema_raw <- ema_raw %>% 
   group_by(id) %>% 
   mutate(day = as.numeric(as.factor(date)),
          .after = minute) %>% 
@@ -44,23 +47,22 @@ data_raw <- data_raw %>%
                             "17" = "3"), .after = day)
   
 #removing data from software errors, unknown sources, and administrative testing account
-data_clean <- data_raw %>% 
+ema_clean <- ema_raw %>% 
   filter((hour == "9" | hour == "13" | hour == "17") & minute == "0") %>% #due to software errors or timezone issues(?), some participants were sent prompts outside of the three scheduled daily timings (9am, 1pm, 5pm), this line of code removes data from all erroneously sent EMAs
   filter(!(is.na(id))) %>% #removes testing data from unknown source(s) that does not match any mobile codes in our key; from mEMA administrators(?)
   filter(!(id == "admin_1")) %>% #removing data from administrative testing account
   filter(!(day > 21)) #due to software errors, some participants were sent EMA prompts after the 21-day study period. This line of code removes this "extra" data
 
 #for documentation: here are the data that the above code removes
-data_dirty <- anti_join(data_raw, data_clean)
-#write.csv(data_dirty, "./data/ema_removed.csv")
+ema_dirty <- anti_join(ema_raw, ema_clean)
+#write.csv(ema_dirty, "./data/ema_removed.csv")
 
 #selecting relevant variables for network estimation
-data_clean <- data_clean %>% 
+ema_clean <- ema_clean %>% 
   select(id, scheduled_start_local, day, occasion, has_data, SE_2, TPESS_1:TPESS_10,SYMP_1:SYMP_12)
-names(data_clean)[6:28] <- variables$label #to clean up labels
+names(ema_clean)[6:28] <- variables$label #to clean up labels
 
 #adding time variable to cleaned data
-data_clean <- data_clean %>% 
+ema_clean <- ema_clean %>% 
   mutate(time = row_number(), .after = scheduled_start_local) #ranges from 1 to 63
-
-#write.csv(data_clean, "./data/ema_clean.csv", row.names = FALSE)
+#write.csv(ema_clean, "./data/ema_clean.csv", row.names = FALSE)
